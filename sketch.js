8c1d34b938702378c5e720e562e1733d618a0a4c
horvathge <ghorvath@femtonics.eu>
2018-11-02 19:54:40 +0100

    Vary brush diameter

--- sketch.js
+++ sketch.js
@@ -6,11 +6,13 @@ class Blob{
         this.G = 0;
         this.B = 0;
 
+        this.d = 20;
+
     }
 
     draw(){
         fill(this.R, this.G, this.B);
-        ellipse(this.x, this.y, 20, 20)
+        ellipse(this.x, this.y, this.d, this.d)
     }
     
     moveTo(x,y){
@@ -23,15 +25,21 @@ class Blob{
         this.G = G;
         this.B = B;
     }
+
+    setDiameter(d){
+        this.d = d;
+    }
 }
 
 var blobs = [new Blob(), new Blob(), new Blob(), new Blob(), new Blob(), new Blob(), new Blob(), new Blob()];
 
 var myX = 0;
 var myY = 0;
+var myD = 0;
 
-var xoff = 0;
-var yoff = 0;
+var xoff = 42;
+var yoff = 73;
+var doff = 17;
 
 var myred = 128;
 var redshift = 1;
@@ -66,6 +74,10 @@ function draw() {
     xoff = xoff + 0.005;
     yoff = yoff + 0.004;
 
+    // Set random diameter 5-25
+    myD = noise(doff)*20+5;
+    doff = doff + 0.006;
+
     // Move blobs to position
     blobs[0].moveTo(myX, myY);
     blobs[1].moveTo(width-myX, myY);
@@ -96,6 +108,7 @@ function draw() {
 
     // Draw with the blobs
     for (var i = 0; i<8; i++){
+        blobs[i].setDiameter(myD);
         blobs[i].setColor(myred, mygreen, myblue);
         blobs[i].draw();
     }
